578e9224abe037d361d94a721d6cf5a8686ad8cb
pedestrianlove <jsli@linux.com>
2025-01-25 18:36:34 +0800

    feat: finish build_chroma

---
+++ scrape.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf rtdocs
+wget -r -l inf -A "*.html" -P rtdocs --no-parent --cut-dirs=4 https://docs.nvidia.com/deeplearning/nccl/user-guide/docs/
+# wget -r -A.html -P rtdocs --no-parent --cut-dirs=4 https://docs.nvidia.com/deeplearning/nccl/user-guide/docs/
