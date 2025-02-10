7747160d88ed95a4000d042e059d3e847d5a7ccc
pedestrianlove <jsli@linux.com>
2025-02-10 17:27:55 +0800

    feat: add hta

--- scrape.sh
+++ scrape.sh
@@ -4,5 +4,6 @@ rm -rf rtdocs
 rm -rf chroma_db
 wget -r -l inf -A "*.html" -P rtdocs --no-parent -X /deeplearning/nccl/archives,/deeplearning/nccl/release-notes,/deeplearning/nccl/sla,/deeplearning/nccl/bsd https://docs.nvidia.com/deeplearning/nccl/
 wget -r -l inf -A "*.html" -P rtdocs --no-parent https://pytorch.org/docs/stable/ 
+wget -r -l inf -A "*.html" -P rtdocs --no-parent https://hta.readthedocs.io/en/latest/
 
 python build_chroma.py
