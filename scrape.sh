03430fd44e8cb462f70558728a70ea03a1041e11
pedestrianlove <jsli@linux.com>
2025-02-19 18:58:38 +0800

    feat: swap hta with ceph docs

--- scrape.sh
+++ scrape.sh
@@ -3,7 +3,8 @@
 rm -rf rtdocs
 rm -rf chroma_db
 wget -r -l inf -A "*.html" -P rtdocs --no-parent -X /deeplearning/nccl/archives,/deeplearning/nccl/release-notes,/deeplearning/nccl/sla,/deeplearning/nccl/bsd https://docs.nvidia.com/deeplearning/nccl/
+wget -r -l inf -A "*.html" -P rtdocs --no-parent https://docs.ceph.com/en/latest/
 wget -r -l inf -A "*.html" -P rtdocs --no-parent https://pytorch.org/docs/stable/ 
-wget -r -l inf -A "*.html" -P rtdocs --no-parent https://hta.readthedocs.io/en/latest/
+#wget -r -l inf -A "*.html" -P rtdocs --no-parent https://hta.readthedocs.io/en/latest/
 
 python build_chroma.py
